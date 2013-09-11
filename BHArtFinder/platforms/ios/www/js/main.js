d73965614a534f46d97f5a39169e8769f09fc401
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-09-11 15:31:50 -0600

    updated instagram test
    code is now identical to the code in the video (bar the client id and
    path to main.js)  still doesn't work.

--- BHArtFinder/platforms/ios/www/js/main.js
+++ BHArtFinder/platforms/ios/www/js/main.js
@@ -9,14 +9,14 @@ $(function() {
 	/*
 	var tag = "graffiti";
 	var url = "https://api.instagram.com/v1/tags" + tag + "media/recent?callback=?&amp;client_id=9a4423b4dfdd4111a73d4bd39082f519&amp;min_id=10"; */
-	$.getJSON(url, feedGrid);
+	$.getJSON(url, screenOutput);
 });
 
-alert("getJSON");
+// alert("getJSON");
 
-var feedGrid = function(info) {
+var screenOutput = function(info) {
 	
-	alert("feedGrid");
+	alert("screenOutput");
 	console.log(info);
 	
 	$("#data-msg").html("<h2>Instagram Results:</h2>");
@@ -25,4 +25,4 @@ var feedGrid = function(info) {
 		var pic = "<li><img src='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + 			", <em>(" + photo.user.name +")</em></h4></li>";
 		$("#data-output").append(pic);
 	}); // end each
-}; // end feedGrid
+}; // end screenOutput
