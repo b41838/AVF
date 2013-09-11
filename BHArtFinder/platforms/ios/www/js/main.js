03bcf9226a1a5adb5a9004ed325f61f58989cea2
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-09-10 19:26:57 -0600

    Testing Instagram API
    skeleton test

--- BHArtFinder/platforms/ios/www/js/main.js
+++ BHArtFinder/platforms/ios/www/js/main.js
@@ -1,3 +1,23 @@
 // JavaScript Document
 // Kyler Schroeder
 // AFV 1309
+
+// Instagram API
+$(function() {
+	// var tag = "bhgraffiti";
+	var tag = "graffiti";
+	var url = "https://api.instagram.com/v1/tags" + tag + "media/recent?callback=?&amp;client_id=9a4423b4dfdd4111a73d4bd39082f519&amp;min_id=10";
+	$.getJSON(URL, feedGrid);
+});
+
+var feedGrid = function(info) {
+	alert("Gimme dem pics!");
+	console.log(info);
+	
+	$("#data-msg").html("<h2>Instagram Results:</h2>");
+	
+	$.each(info.data, function(index, photo) {  // index is position in array of info.data
+		var pic = "<li><img src='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + 			", <em>(" + photo.user.name +")</em></h4></li>";
+		$("#data-output").append(pic);
+	}); // end each
+}; // end feedGrid
