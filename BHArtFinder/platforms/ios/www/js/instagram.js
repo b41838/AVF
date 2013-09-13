fbbe9a2d58cc97c27f58cec72e5a5c449d864133
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-09-12 19:47:29 -0600

    Test Pages
    sending working test pages to Jen for troubleshooting.

---
+++ BHArtFinder/platforms/ios/www/js/instagram.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+	// Instagram API
+	$(function() {
+		// var tag = "bhgraffiti";
+		var tag = "graffiti";
+		var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=9a4423b4dfdd4111a73d4bd39082f519&amp;count=12";
+		$.getJSON(url, screenOutput);
+		
+	});
+	
+	// alert("getJSON");
+	
+	var screenOutput = function(info) {
+		
+		//alert("screenOutput");
+		//console.log(data);
+		console.log(info);
+		
+		$("#data-msg").html("<h2>Instagram Results:</h2>");
+		
+		$.each(info.data, function(index, photo) {  // index is position in array of info.data
+			var pic = "<li><img src='" + photo.images.thumbnail.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + 			", <em>(" + photo.user.name +")</em></h4></li>";
+			$("#data-output").append(pic);
+		}); // end each
+		
+		$("li:nth-child(3n+1)").addClass("ui-block-a");
+		$("li:nth-child(3n+2)").addClass("ui-block-b");
+		$("li:nth-child(3n+3)").addClass("ui-block-c");
+	}; // end screenOutput
+});
