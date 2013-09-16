3230c3eef790d047885240781b830c374c98b3b8
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-09-16 15:18:16 -0600

    Uncommitted Changes
    Changes that didn't get sync'd for Friday's turn in.

--- BHArtFinder/platforms/ios/www/js/main.js
+++ BHArtFinder/platforms/ios/www/js/main.js
@@ -10,18 +10,18 @@ $('#instagramFeed').on('pageinit', function() {
 		var tag = "graffiti";
 		var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=9a4423b4dfdd4111a73d4bd39082f519&amp;count=12";
 		
-		$.getJSON(url, screenOutput);
+		$.getJSON(url, getFeed);
 });
 	
-	var screenOutput = function(info) {
+	var getFeed = function(info) {
 
 		console.log(info);
 		
-		$("#data-msg").html("<h2>Instagram Results:</h2>");
+		$("#instagramStatus").html("<h2>Instagram Results:</h2>");
 		
 		$.each(info.data, function(index, photo) {  // index is position in array of info.data
 			var pic = "<li><img src='" + photo.images.thumbnail.url + "' alt='" + photo.user.id + "' /><h4>" + photo.user.full_name + 			", <em>(" + photo.user.name +")</em></h4></li>";
-			$("#data-output").append(pic);
+			$("#pullFeed").append(pic);
 		}); // end each
 		
 		$("li:nth-child(3n+1)").addClass("ui-block-a");
